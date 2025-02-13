af9d5f08a69d027cf852a1b29dbbb59d647b52d0
dashath0097 <dashathk0440@gmail.com>
2025-02-13 20:24:54 +0530

    Create lb.tf

---
+++ lb.tf
@@ -0,0 +1,171 @@
+provider "azurerm" {
+  features {}
+}
+
+# Define variables for customization
+variable "location" {
+  default = "East US"
+}
+
+variable "resource_group_name" {
+  default = "myResourceGroup"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
+
+variable "admin_password" {
+  default = "Dashath@898989"
+}
+
+# Create Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Create Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create Public IP for Load Balancer
+resource "azurerm_public_ip" "lb_pip" {
+  name                = "lbPublicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Create Load Balancer
+resource "azurerm_lb" "lb" {
+  name                = "myLoadBalancer"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "lbFrontend"
+    public_ip_address_id = azurerm_public_ip.lb_pip.id
+  }
+}
+
+# Create Backend Pool for Load Balancer
+resource "azurerm_lb_backend_address_pool" "lb_backend_pool" {
+  loadbalancer_id = azurerm_lb.lb.id
+  name            = "myBackendPool"
+}
+
+# Create Health Probe
+resource "azurerm_lb_probe" "lb_probe" {
+  loadbalancer_id = azurerm_lb.lb.id
+  name            = "http_probe"
+  protocol        = "Tcp"
+  port           = 80
+}
+
+# Create Load Balancer Rule
+resource "azurerm_lb_rule" "lb_rule" {
+  loadbalancer_id                = azurerm_lb.lb.id
+  name                           = "http_rule"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "lbFrontend"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lb_backend_pool.id]
+  probe_id                       = azurerm_lb_probe.lb_probe.id
+}
+
+# Network Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "myNSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create NICs for VMs
+resource "azurerm_network_interface" "nic" {
+  count               = 2
+  name                = "vm-nic-${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Associate NICs with Backend Pool
+resource "azurerm_network_interface_backend_address_pool_association" "nic_backend" {
+  count                   = 2
+  network_interface_id    = azurerm_network_interface.nic[count.index].id
+  backend_address_pool_id = azurerm_lb_backend_address_pool.lb_backend_pool.id
+}
+
+# Create Virtual Machines
+resource "azurerm_windows_virtual_machine" "vm" {
+  count               = 2
+  name                = "myVM-${count.index}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+
+  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+
+output "lb_public_ip" {
+  value = azurerm_public_ip.lb_pip.ip_address
+}
